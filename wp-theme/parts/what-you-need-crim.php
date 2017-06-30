1704a8b4819a622fcc7fee6e382b286bd6f105f5
haa-gg <glen.gengel@hoglundanalytics.com>
2017-06-30 13:53:03 -0500

    Typo and order changes

--- wp-theme/parts/what-you-need-crim.php
+++ wp-theme/parts/what-you-need-crim.php
@@ -43,6 +43,50 @@
 
     </div>
 
+    <div class="row r3-r4">
+
+        <div class="columns small-1 blue-bg">
+            &nbsp;
+        </div>
+
+        <div class="columns small-11 large-11 qs">
+
+        <h3 class="show-for-medium">What is an aggravating factor for DWI in Minnesota?</h3>
+
+            <div class="row">
+
+                <div class="columns small-9 large-9">
+
+<h3 class="hide-for-medium">What is an aggravating factor for DWI in Minnesota?</h3>
+
+                   <p class="desk-show wyn-statement-2">The state of Minnesota utilizes aggravating factors to increase charges and penalties for DWI.
+                    Aggravating factors are:  
+
+                </p><ul id="wyn-reveal-2">
+                <li>A prior impaired driving conviction111</li>
+                <li>Driving with a child under 16 in the vehicle</li>
+                <li>Driving with an alcohol concentration of .16 or higher</li>
+            </ul>
+
+
+        </div>
+
+        <div class="columns small-3 large-3 cta align-middle">
+
+            <div class="bot-align">
+
+                <a href="#/" id="wyn-toggle-2" class="haa-track">Read More</a>
+
+            </div>
+
+        </div>
+
+    </div>
+
+</div>
+
+</div>
+
 
 <div class="row r3-r2">
 
@@ -89,13 +133,13 @@
 
     <div class="columns small-11 large-11 qs">
 
-     <h3 class="show-for-medium">Help, they’ve take my car and are forfeiting it. Why?</h3>
+     <h3 class="show-for-medium">Help, they’ve taken my car and are forfeiting it. Why?</h3>
 
      <div class="row">
 
         <div class="columns small-9 large-9">
 
-        <h3 class="hide-for-medium">Help, they’ve take my car and are forfeiting it. Why?</h3>
+        <h3 class="hide-for-medium">Help, they’ve taken my car and are forfeiting it. Why?</h3>
 
             <p class="desk-show wyn-statement-4">The state of Minnesota will seize and forfeit a motor vehicle for several offenses including a DWI with any two aggravating factors. 
                 <span id="wyn-reveal-4">
@@ -120,48 +164,6 @@
 
 </div>
 
-<div class="row r3-r4">
 
-        <div class="columns small-1 blue-bg">
-            &nbsp;
-        </div>
-
-        <div class="columns small-11 large-11 qs">
-
-        <h3 class="show-for-medium">What is an aggravating factor for DWI in Minnesota?</h3>
-
-            <div class="row">
-
-                <div class="columns small-9 large-9">
-
-<h3 class="hide-for-medium">What is an aggravating factor for DWI in Minnesota?</h3>
-
-                   <p class="desk-show wyn-statement-2">The state of Minnesota utilizes aggravating factors to increase charges and penalties for DWI.
-                    Aggravating factors are:  
-
-                </p><ul id="wyn-reveal-2">
-                <li>A prior impaired driving conviction111</li>
-                <li>Driving with a child under 16 in the vehicle</li>
-                <li>Driving with an alcohol concentration of .16 or higher</li>
-            </ul>
-
-
-        </div>
-
-        <div class="columns small-3 large-3 cta align-middle">
-
-            <div class="bot-align">
-
-                <a href="#/" id="wyn-toggle-2" class="haa-track">Read More</a>
-
-            </div>
-
-        </div>
-
-    </div>
-
-</div>
-
-</div>
 
 </div>
